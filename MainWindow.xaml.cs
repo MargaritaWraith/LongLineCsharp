f7c027b009cb5cde26d7f3c71b3f22b7dd3a2124
Kondr <Kondr@ANDROMEDA>
2020-11-09 19:35:21 +0300

    Центровка окна. Корректировка размеров. Вывод графика тока.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -35,24 +35,26 @@ namespace LongLine
             var Zn = new Complex(double.Parse(RnEdit.Text), double.Parse(XnEdit.Text));
             //MessageBox.Show($"Zн = {Zn.Real} + j{Zn.Imaginary}");
 
-            var LongLine = new LLine(150, 0, 150, 10);
+            var LongLine = new LLine(100, 0, 150, 10);
             var (z, Uz, Iz) = LongLine.Solve(1, 2);
 
 
             var UzGraph = new LineSeries();
+            var IzGraph = new LineSeries();
 
             for (int i = 0; i < z.Length; i++)
             {
                 UzGraph.Points.Add(new DataPoint(z[i], Uz[i].Magnitude));
+                IzGraph.Points.Add(new DataPoint(z[i], Iz[i].Magnitude * 1000));
             }
 
             UzGraph.Color = OxyColors.Red;
             UzGraph.StrokeThickness = 2;
 
-            var model = new PlotModel();
-            model.Title = "U(z)";
-            model.Series.Add(UzGraph);
-            model.Axes.Add(new LinearAxis
+            var Uz_model = new PlotModel();
+            Uz_model.Title = "U(z)";
+            Uz_model.Series.Add(UzGraph);
+            Uz_model.Axes.Add(new LinearAxis
             {
                 Title = "z, м",
                 Position = AxisPosition.Bottom,
@@ -65,7 +67,7 @@ namespace LongLine
                 MajorGridlineStyle = LineStyle.Solid,
                 MinorGridlineStyle = LineStyle.Dash,
             });
-            model.Axes.Add(new LinearAxis
+            Uz_model.Axes.Add(new LinearAxis
             {
                 Title = "|U(z)|, В",
                 Position = AxisPosition.Left,
@@ -79,8 +81,41 @@ namespace LongLine
                 MinorGridlineStyle = LineStyle.Dash,
             });
 
-            PlotUz.Model = model;
-            
+            PlotUz.Model = Uz_model;
+
+
+            var Iz_model = new PlotModel();
+            Iz_model.Title = "I(z)";
+            Iz_model.Series.Add(IzGraph);
+            Iz_model.Axes.Add(new LinearAxis
+            {
+                Title = "z, м",
+                Position = AxisPosition.Bottom,
+                Minimum = 0,
+                Maximum = z.Max(),
+                MajorGridlineThickness = 1,
+                MinorGridlineThickness = 0.5,
+                MajorGridlineColor = OxyColors.Green,
+                MinorGridlineColor = OxyColors.Green,
+                MajorGridlineStyle = LineStyle.Solid,
+                MinorGridlineStyle = LineStyle.Dash,
+            });
+            Iz_model.Axes.Add(new LinearAxis
+            {
+                Title = "|I(z)|, мА",
+                Position = AxisPosition.Left,
+                Minimum = 0,
+                Maximum = 1.2 * Iz.Max(u => u.Magnitude) * 1000,
+                MajorGridlineThickness = 1,
+                MinorGridlineThickness = 0.5,
+                MajorGridlineColor = OxyColors.Green,
+                MinorGridlineColor = OxyColors.Green,
+                MajorGridlineStyle = LineStyle.Solid,
+                MinorGridlineStyle = LineStyle.Dash,
+            });
+
+            PlotIz.Model = Iz_model;
+
 
         }
     }
